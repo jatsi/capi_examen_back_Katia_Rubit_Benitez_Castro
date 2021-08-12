618dbe18cd5d2f37303c89091572a0d80feb4b58
jatsi <33884168+jatsi@users.noreply.github.com>
2021-08-12 15:04:58 -0500

    Database

---
+++ database/migrations/2021_08_12_195556_create_user_domicilios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('domicilio');
+            $table->integer('numero_exterior');
+            $table->string('colonia');
+            $table->integer('cp');
+            $table->string('ciudad');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilios');
+    }
+}
